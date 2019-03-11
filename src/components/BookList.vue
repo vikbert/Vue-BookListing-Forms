36f5224d7016066345410fcee1ebc5157b03d92d
Xun Zhou <xun.zhou@lidl.com>
2019-03-11 15:00:44 +0100

    add search input

--- src/components/BookList.vue
+++ src/components/BookList.vue
@@ -1,8 +1,9 @@
 <template>
   <div>
     <h1>{{title}}</h1>
+    <input type="text" placeholder="search books" v-model="searchInput">
     <ul class="exists">
-      <book-item v-for="book in books" :key="book.id" :book="book"></book-item>
+      <book-item v-for="book in searchedBooks" :key="book.id" :book="book"></book-item>
     </ul>
     <hr>
     <h2>Filtered books by Ownership</h2>
@@ -50,7 +51,8 @@ export default {
         }
       ],
       filters: ["bought", "borrowed"],
-      holding: "bought"
+      holding: "bought",
+      searchInput: ""
     };
   },
   components: {
@@ -60,6 +62,13 @@ export default {
   computed: {
     filteredBooks() {
       return _.filter(this.books, ["ownership", this.holding]);
+    },
+    searchedBooks() {
+      const searchFilter = book => {
+        return book.title.toLowerCase().match(this.searchInput.toLowerCase());
+      };
+
+      return _.filter(this.books, searchFilter);
     }
   },
   methods: {
